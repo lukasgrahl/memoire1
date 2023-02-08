1b6a99dc5f83109b798d31440a5b51dd54bc8ac2
Lukas Grahl <lukas.g@amkat.de>
2023-02-08 13:07:30 +0100

    added model_files dir to paths

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import os
 
-PROJECT_ROOT = os.getcdr()
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
+MODEL_DIR = os.path.join(PROJECT_ROOT, 'model_files')
